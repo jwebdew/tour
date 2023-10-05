e775b0955916ddcf18e8f8a3f9846d09eeeda592
hasujeong <75551721+jwebdew@users.noreply.github.com>
2023-10-05 15:56:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './style/App.css';
+import TourList from './TourList';
+
+function App() {
+  return (
+    <div className="App">
+       <TourList />
+    </div>
+  );
+}
+
+export default App;
